a48c5fda26685ff00351006b6b98375413496cff
Paradockz <86320609+Paradockz@users.noreply.github.com>
2026-06-17 17:09:13 -0400

    Basic rudimentary PSR-14 event provider/dispatcher

---
+++ src/Event/Dispatcher.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Darflen\Framework\Event;
+
+use Override;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
+use Psr\EventDispatcher\StoppableEventInterface;
+
+class Dispatcher implements EventDispatcherInterface
+{
+    private ListenerProviderInterface $provider;
+
+    public function __construct(ListenerProviderInterface $provider)
+    {
+        $this->provider = $provider;
+    }
+
+    #[Override]
+    public function dispatch(object $event)
+    {
+        $listeners = $this->provider->getListenersForEvent($event);
+        /** @disregard */
+        foreach ($listeners as $listener) {
+            if ($event instanceof StoppableEventInterface && $event->isPropagationStopped()) {
+                break;
+            }
+            $listener($event);
+        }
+    }
+}
---
+++ src/Event/Provider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Darflen\Framework\Event;
+
+use Override;
+use Psr\EventDispatcher\ListenerProviderInterface;
+
+class Provider implements ListenerProviderInterface
+{
+    private array $listeners = [];
+
+    public function setListenerForEvent(string $eventClassName, callable $listener): void
+    {
+        $this->listeners[$eventClassName][] = $listener;
+    }
+
+    #[Override]
+    public function getListenersForEvent(object $event): iterable
+    {
+        foreach ($this->listeners as $eventClassName => $listeners) {
+            if ($event instanceof $eventClassName) {
+                foreach ($listeners as $listener) {
+                    yield $listener;
+                }
+            }
+        }
+    }
+}
