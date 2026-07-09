2833e50ef414d0470c42f4e34b1ab3409219c077
Paradockz <86320609+Paradockz@users.noreply.github.com>
2026-07-08 21:33:06 -0400

    Updated path normalization helper

--- src/Support/Helpers.php
+++ src/Support/Helpers.php
@@ -14,9 +14,37 @@ function config(string $key, mixed $default = ''): mixed
     return Config::get($key, $default);
 }
 
+function getDrivePath(string $path): string
+{
+    $drive = '';
+    if (preg_match('/^[a-zA-Z]:/', $path)) {
+        $drive = substr($path, 0, 2) . '/';
+    } elseif (strpos($path, '/') === 0) {
+        $drive = '/';
+    }
+    return $drive;
+}
+
 function normalizePath(string $path): string
 {
-    $path = str_replace(["/", "\\"], DIRECTORY_SEPARATOR, $path);
-    $path = preg_replace('#[\\\/]+#', DIRECTORY_SEPARATOR, $path);
-    return trim($path, DIRECTORY_SEPARATOR);
+    $drive = getDrivePath($path);
+    if ($drive !== '/' && $drive !== '') {
+        $path = substr($path, 2);
+    }
+    $path = str_replace(["/", "\\"], '/', $path);
+    $path = preg_replace('#[\\\/]+#', '/', $path);
+    $parts = explode('/', $path);
+    $absolutes = [];
+    foreach ($parts as $part) {
+        if ($part === '.' || $part === '') {
+            continue;
+        }
+        if ($part === '..') {
+            array_pop($absolutes);
+        } else {
+            $absolutes[] = $part;
+        }
+    }
+    $path = $drive . implode('/', $absolutes);
+    return $path;
 }
---
+++ tests/Support/HelpersTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Darflen\Framework\Tests\Support;
+
+use PHPUnit\Framework\TestCase;
+use Generator;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+class HelpersTest extends TestCase
+{
+    public static function fullPathDataProvider(): Generator
+    {
+        $data = [
+            '/foo/bar' => '/foo/bar',
+            '/foo/./bar' => '/foo/bar',
+            '/foo/../bar' => '/bar',
+            'foo/bar' => 'foo/bar',
+            'foo/./bar' => 'foo/bar',
+            'foo/../bar' => 'bar',
+            '/foo/fizzbuzz.php' => '/foo/fizzbuzz.php',
+            '/foo/../fizzbuzz.php' => '/fizzbuzz.php',
+            'foo/fizzbuzz.php' => 'foo/fizzbuzz.php',
+            'foo/../fizzbuzz.php' => 'fizzbuzz.php',
+            'foo/./bar/baz/../foobar.php' => 'foo/bar/foobar.php',
+            'C:/foo\././.\../fizzbuzz.php' => 'C:/fizzbuzz.php',
+            '/../../../../foo/bar' => '/foo/bar',
+            'foo///bar\\\\foobar.php' => 'foo/bar/foobar.php',
+            'D:\\foo\\bar' => 'D:/foo/bar'
+        ];
+
+        foreach ($data as $input => $expected) {
+            yield [$input, $expected];
+        }
+    }
+
+    #[DataProvider('fullPathDataProvider')]
+    public function testNormalizePathWithDefaultMode(string $input, string $expected)
+    {
+        $this->assertSame($expected, normalizePath($input));
+    }
+}
